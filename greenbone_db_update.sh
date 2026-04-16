edcb7327fbafa064177fd17e0c5ae2ed6f158c0b
grizzledbadger0806 <timothy.k.fournier+github@gmail.com>
2026-04-16 17:03:59 -0500

    updated path

--- greenbone_db_update.sh
+++ greenbone_db_update.sh
@@ -7,6 +7,6 @@ docker compose -f /home/$(whoami)/greenbone-community-container/docker-compose.y
 ##Signature UpdateS
 docker compose -f /home/$(whoami)/greenbone-community-container/docker-compose.yml -p greenbone-community-edition pull notus-data vulnerability-tests scap-data dfn-cert-data cert-bund-data report-formats data-objects
 
-docker compose -f /home/honeybadger/greenbone-community-container/docker-compose.yml -p greenbone-community-edition up -d notus-data vulnerability-tests scap-data dfn-cert-data cert-bund-data report-formats data-objects
+docker compose -f /home/$(whoami)/greenbone-community-container/docker-compose.yml -p greenbone-community-edition up -d notus-data vulnerability-tests scap-data dfn-cert-data cert-bund-data report-formats data-objects
 
 docker system prune -af --volumes
